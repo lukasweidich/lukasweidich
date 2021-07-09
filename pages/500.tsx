16950dc5cf73db807e6771aaf0c2dbfcb276e8d7
Lukas <lukas.weidich@gmx.de>
2021-07-09 10:03:52 +0200

    🔧 update .eslintrc

---
+++ pages/500.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ServerErrorPage = () => {
+	return <div>500 :(</div>;
+};
+
+export default ServerErrorPage;
---
+++ pages/_offline.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const _offline = () => {
+	return <div>You&aposre offline.</div>;
+};
+
+export default _offline;
